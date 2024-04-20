8927642d9d94dc5a3b9f0e9835ab0090b77035aa
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2024-04-20 12:39:03 -0300

    mensagem nova

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teste de Rede</title>
+</head>
+<body>
+
 <?php
-    echo "Olá, Mundo! Este é o teste para o trabalho extra de 5 pontos da disciplina de redes.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    echo "<p>Olá, $nome! Este é o teste para o trabalho extra de 5 pontos da disciplina de redes.</p>";
+}
 ?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="nome">Informe seu nome:</label><br>
+    <input type="text" id="nome" name="nome"><br><br>
+    <input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
